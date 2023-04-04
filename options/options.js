f9a65d4e69d2d502efcc46eedc543d27a3048cfc
Crowdbotics <team@crowdbotics.com>
2023-04-04 15:14:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-waterfall-39703",
+  url: "https://polished-waterfall-39703.botics.co",
+  api: "https://polished-waterfall-39703.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
